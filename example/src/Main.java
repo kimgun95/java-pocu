9c30e22a3b1e4ec80d78cb77ea76239463569986
kimgun95 <harrison1995@naver.com>
2021-01-11 21:41:48 +0900

    practice a public access modifier

---
+++ example/src/Main.java
@@ -0,0 +1,10 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.name = "Chris";
+        System.out.println(person.name);
+//        person.beAwesome();
+    }
+}
---
+++ example/src/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+    public int age;
+    String name;
+
+    public void beAwesome() {
+        this.name = "James";
+        System.out.println(this.name);
+    }
+}
