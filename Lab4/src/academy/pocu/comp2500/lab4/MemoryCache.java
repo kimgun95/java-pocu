2e5595fbbd0259cada40e1e207efba4459ef601f
kimgun95 <harrison1995@naver.com>
2021-01-29 23:52:42 +0900

    I coded the lab4 using multiton

--- Lab4/src/academy/pocu/comp2500/lab4/MemoryCache.java
+++ Lab4/src/academy/pocu/comp2500/lab4/MemoryCache.java
@@ -1,37 +1,160 @@
 package academy.pocu.comp2500.lab4;
 
-import java.time.OffsetDateTime;
 import java.util.*;
 
 public class MemoryCache {
     private static HashMap<String, MemoryCache> instance = new HashMap<String, MemoryCache>();
+    // 무조건 입력 순서대로
+    private static LinkedList<String> instanceLinkedList = new LinkedList<>();
+    // 무조건 수정 순서대로
+    private static LinkedHashSet<String> instanceLinkedHastSet = new LinkedHashSet<>();
     private static int memoryCacheMaxSize = 10;
-    private OffsetDateTime modifiedDateTime;
+    private static EvictionPolicy evictionPolicy = EvictionPolicy.LEAST_RECENTLY_USED;
+    private HashMap<String, String> entry = new HashMap<>();
+    private LinkedList<String> entryLinkedList = new LinkedList<>();
+    private LinkedHashSet<String> entryLinkedHashSet = new LinkedHashSet<>();
+    private int entryMaxSize = 10;
 
     private MemoryCache() {
-        this.modifiedDateTime = OffsetDateTime.now();
     }
     public static MemoryCache getInstance(String hardDiskName) {
         if (!instance.containsKey(hardDiskName)) {
-            if (memoryCacheMaxSize == instance.size()) {
-                List<Map.Entry<String, MemoryCache>> entries = new LinkedList<>(instance.entrySet());
-                Collections.sort(entries, (t1, t2) -> t2.getValue().modifiedDateTime.compareTo(t1.getValue().modifiedDateTime));
-                instance.remove(entries.get(0).getKey());
+            if (MemoryCache.memoryCacheMaxSize == instance.size()) {
+                if (evictionPolicy == EvictionPolicy.FIRST_IN_FIRST_OUT) {
+                    instance.remove(instanceLinkedList.getFirst());
+                    instanceLinkedHastSet.remove(instanceLinkedList.getFirst());
+                    instanceLinkedList.remove(0);
+                } else if (evictionPolicy == EvictionPolicy.LAST_IN_FIRST_OUT) {
+                    instance.remove(instanceLinkedList.getLast());
+                    instanceLinkedHastSet.remove(instanceLinkedList.getLast());
+                    instanceLinkedList.remove(instanceLinkedList.size() - 1);
+                } else {
+                    instance.remove(instanceLinkedHastSet.iterator().next());
+                    int cnt = 0;
+                    for (String str : instanceLinkedList) {
+                        if (str.equals(instanceLinkedHastSet.iterator().next())) {
+                            instanceLinkedList.remove(cnt);
+                            break;
+                        }
+                        cnt += 1;
+                    }
+                    instanceLinkedHastSet.remove(instanceLinkedHastSet.iterator().next());
+                }
             }
             instance.put(hardDiskName, new MemoryCache());
+            instanceLinkedList.add(hardDiskName);
+        } else {
+            instanceLinkedHastSet.remove(hardDiskName);
         }
-        System.out.println("hi" + instance);
+        instanceLinkedHastSet.add(hardDiskName);
+        //System.out.println("inst" + instance);
+        //System.out.println("LRU" + instanceLinkedHastSet);
+        //System.out.println("FIFO" + instanceLinkedList);
         return instance.get(hardDiskName);
     }
     public void clear() {
-        ArrayList<String> keyList = new ArrayList<String>();
-        keyList.addAll(instance.keySet());
-        for (String key : keyList) {
-            instance.remove(key);
-        }
+        instance.clear();
+        instanceLinkedList.clear();
+        instanceLinkedHastSet.clear();
     }
     public void setMaxInstanceCount(int size) {
         memoryCacheMaxSize = size;
+        while (memoryCacheMaxSize < instance.size()) {
+            if (evictionPolicy == EvictionPolicy.FIRST_IN_FIRST_OUT) {
+                instance.remove(instanceLinkedList.getFirst());
+                instanceLinkedHastSet.remove(instanceLinkedList.getFirst());
+                instanceLinkedList.remove(0);
+            } else if (evictionPolicy == EvictionPolicy.LAST_IN_FIRST_OUT) {
+                instance.remove(instanceLinkedList.getLast());
+                instanceLinkedHastSet.remove(instanceLinkedList.getLast());
+                instanceLinkedList.remove(instanceLinkedList.size() - 1);
+            } else {
+                instance.remove(instanceLinkedHastSet.iterator().next());
+                int cnt = 0;
+                for (String str : instanceLinkedList) {
+                    if (str.equals(instanceLinkedHastSet.iterator().next())) {
+                        instanceLinkedList.remove(cnt);
+                        break;
+                    }
+                    cnt += 1;
+                }
+                instanceLinkedHastSet.remove(instanceLinkedHastSet.iterator().next());
+            }
+        }
     }
-
+    public void setEvictionPolicy(EvictionPolicy evictPolicy) {
+        evictionPolicy = evictPolicy;
+    }
+    public void addEntry(String key, String value) {
+        if (!this.entry.containsKey(key)) {
+            if (entryMaxSize == entry.size()) {
+                if (evictionPolicy == EvictionPolicy.FIRST_IN_FIRST_OUT) {
+                    entry.remove(entryLinkedList.getFirst());
+                    entryLinkedHashSet.remove(entryLinkedList.getFirst());
+                    entryLinkedList.remove(0);
+                } else if (evictionPolicy == EvictionPolicy.LAST_IN_FIRST_OUT) {
+                    entry.remove(entryLinkedList.getLast());
+                    entryLinkedHashSet.remove(entryLinkedList.getLast());
+                    entryLinkedList.remove(entryLinkedList.size() - 1);
+                } else {
+                    entry.remove(entryLinkedHashSet.iterator().next());
+                    int cnt = 0;
+                    for (String str : entryLinkedList) {
+                        if (str.equals(entryLinkedHashSet.iterator().next())) {
+                            entryLinkedList.remove(cnt);
+                            break;
+                        }
+                        cnt += 1;
+                    }
+                    entryLinkedHashSet.remove(entryLinkedHashSet.iterator().next());
+                }
+            }
+            entry.put(key, value);
+            entryLinkedList.add(key);
+        } else {
+            entry.put(key, value);
+            entryLinkedHashSet.remove(key);
+        }
+        entryLinkedHashSet.add(key);
+        //System.out.println("inst" + entry);
+        //System.out.println("LRU" + entryLinkedHashSet);
+        //System.out.println("FIFO" + entryLinkedList);
+    }
+    public void setMaxEntryCount(int size) {
+        this.entryMaxSize = size;
+        while (entryMaxSize < entry.size()) {
+            if (evictionPolicy == EvictionPolicy.FIRST_IN_FIRST_OUT) {
+                entry.remove(entryLinkedList.getFirst());
+                entryLinkedHashSet.remove(entryLinkedList.getFirst());
+                entryLinkedList.remove(0);
+            } else if (evictionPolicy == EvictionPolicy.LAST_IN_FIRST_OUT) {
+                entry.remove(entryLinkedList.getLast());
+                entryLinkedHashSet.remove(entryLinkedList.getLast());
+                entryLinkedList.remove(entryLinkedList.size() - 1);
+            } else {
+                entry.remove(entryLinkedHashSet.iterator().next());
+                int cnt = 0;
+                for (String str : entryLinkedList) {
+                    if (str.equals(entryLinkedHashSet.iterator().next())) {
+                        entryLinkedList.remove(cnt);
+                        break;
+                    }
+                    cnt += 1;
+                }
+                entryLinkedHashSet.remove(entryLinkedHashSet.iterator().next());
+            }
+        }
+        //System.out.println("inst" + entry);
+        //System.out.println("LRU" + entryLinkedHashSet);
+        //System.out.println("FIFO" + entryLinkedList);
+    }
+    public String getEntryOrNull(String key) {
+        if (!entry.containsKey(key)) {
+            return null;
+        } else {
+            entryLinkedHashSet.remove(key);
+            entryLinkedHashSet.add(key);
+            return entry.get(key);
+        }
+    }
 }
