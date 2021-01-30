633b1d2e5a27b3516a67af02b4a6a85e76f6655c
kimgun95 <harrison1995@naver.com>
2021-01-30 17:30:33 +0900

    modify Instance remove rule

--- Lab4/src/academy/pocu/comp2500/lab4/MemoryCache.java
+++ Lab4/src/academy/pocu/comp2500/lab4/MemoryCache.java
@@ -22,27 +22,27 @@ public class MemoryCache {
     public static MemoryCache getInstance(String hardDiskName) {
         if (!instance.containsKey(hardDiskName)) {
             while (MemoryCache.memoryCacheMaxSize <= instance.size()) {
-                if (evictionPolicy == EvictionPolicy.FIRST_IN_FIRST_OUT) {
-                    instance.remove(instanceLinkedList.getFirst());
-                    instanceLinkedHashSet.remove(instanceLinkedList.getFirst());
-                    instanceLinkedList.remove(0);
-                } else if (evictionPolicy == EvictionPolicy.LAST_IN_FIRST_OUT) {
-                    instance.remove(instanceLinkedList.getLast());
-                    instanceLinkedHashSet.remove(instanceLinkedList.getLast());
-                    instanceLinkedList.remove(instanceLinkedList.size() - 1);
-                } else {
-                    instance.remove(instanceLinkedHashSet.iterator().next());
-                    int cnt = 0;
-                    for (String str : instanceLinkedList) {
-                        if (str.equals(instanceLinkedHashSet.iterator().next())) {
-                            instanceLinkedList.remove(cnt);
-                            break;
-                        }
-                        cnt += 1;
+//                if (evictionPolicy == EvictionPolicy.FIRST_IN_FIRST_OUT) {
+//                    instance.remove(instanceLinkedList.getFirst());
+//                    instanceLinkedHashSet.remove(instanceLinkedList.getFirst());
+//                    instanceLinkedList.remove(0);
+//                } else if (evictionPolicy == EvictionPolicy.LAST_IN_FIRST_OUT) {
+//                    instance.remove(instanceLinkedList.getLast());
+//                    instanceLinkedHashSet.remove(instanceLinkedList.getLast());
+//                    instanceLinkedList.remove(instanceLinkedList.size() - 1);
+//                } else {
+                instance.remove(instanceLinkedHashSet.iterator().next());
+                int cnt = 0;
+                for (String str : instanceLinkedList) {
+                    if (str.equals(instanceLinkedHashSet.iterator().next())) {
+                        instanceLinkedList.remove(cnt);
+                        break;
                     }
-                    instanceLinkedHashSet.remove(instanceLinkedHashSet.iterator().next());
+                    cnt += 1;
                 }
+                instanceLinkedHashSet.remove(instanceLinkedHashSet.iterator().next());
             }
+//            }
             instance.put(hardDiskName, new MemoryCache());
             instanceLinkedList.add(hardDiskName);
         } else {
@@ -62,26 +62,26 @@ public class MemoryCache {
     public static void setMaxInstanceCount(int size) {
         memoryCacheMaxSize = size;
         while (memoryCacheMaxSize < instance.size()) {
-            if (evictionPolicy == EvictionPolicy.FIRST_IN_FIRST_OUT) {
-                instance.remove(instanceLinkedList.getFirst());
-                instanceLinkedHashSet.remove(instanceLinkedList.getFirst());
-                instanceLinkedList.remove(0);
-            } else if (evictionPolicy == EvictionPolicy.LAST_IN_FIRST_OUT) {
-                instance.remove(instanceLinkedList.getLast());
-                instanceLinkedHashSet.remove(instanceLinkedList.getLast());
-                instanceLinkedList.remove(instanceLinkedList.size() - 1);
-            } else {
-                instance.remove(instanceLinkedHashSet.iterator().next());
-                int cnt = 0;
-                for (String str : instanceLinkedList) {
-                    if (str.equals(instanceLinkedHashSet.iterator().next())) {
-                        instanceLinkedList.remove(cnt);
-                        break;
-                    }
-                    cnt += 1;
+//            if (evictionPolicy == EvictionPolicy.FIRST_IN_FIRST_OUT) {
+//                instance.remove(instanceLinkedList.getFirst());
+//                instanceLinkedHashSet.remove(instanceLinkedList.getFirst());
+//                instanceLinkedList.remove(0);
+//            } else if (evictionPolicy == EvictionPolicy.LAST_IN_FIRST_OUT) {
+//                instance.remove(instanceLinkedList.getLast());
+//                instanceLinkedHashSet.remove(instanceLinkedList.getLast());
+//                instanceLinkedList.remove(instanceLinkedList.size() - 1);
+//            } else {
+            instance.remove(instanceLinkedHashSet.iterator().next());
+            int cnt = 0;
+            for (String str : instanceLinkedList) {
+                if (str.equals(instanceLinkedHashSet.iterator().next())) {
+                    instanceLinkedList.remove(cnt);
+                    break;
                 }
-                instanceLinkedHashSet.remove(instanceLinkedHashSet.iterator().next());
+                cnt += 1;
             }
+            instanceLinkedHashSet.remove(instanceLinkedHashSet.iterator().next());
+//            }
         }
     }
     public void setEvictionPolicy(EvictionPolicy evictPolicy) {
