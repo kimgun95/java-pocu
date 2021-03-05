0751cbe9c33a863dffe059089cbc9fdc48526b41
kimgun95 <harrison1995@naver.com>
2021-03-05 11:19:08 +0900

    modify hashCode()

--- Lab7/src/academy/pocu/comp2500/lab7/Bookshelf.java
+++ Lab7/src/academy/pocu/comp2500/lab7/Bookshelf.java
@@ -33,8 +33,8 @@ public final class Bookshelf {
         }
         return false;
     }
-//    @Override
-//    public int hashCode() {
-//        return this.getClass().hashCode() * 31;
-//    }
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
