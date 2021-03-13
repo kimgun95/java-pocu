fafeaa67a9423b577a4aa9f65cc75c27ad05c93c
kimgun95 <harrison1995@naver.com>
2021-03-14 02:50:22 +0900

    ass 3

---
+++ Assignment3/src/academy/pocu/comp2500/assignment3/SimulationManager.java
@@ -0,0 +1,33 @@
+package academy.pocu.comp2500.assignment3;
+
+import java.util.ArrayList;
+
+public class SimulationManager {
+    public static SimulationManager getInstance() {
+        return null;
+    }
+
+    public ArrayList<Unit> getUnits() {
+        return null;
+    }
+
+    public void spawn(Unit unit) {
+
+    }
+    // 매개변수 자료형 변경 가능
+    public void registerThinkable(Unit thinkable) {
+
+    }
+    // 매개변수 자료형 변경 가능
+    public void registerMovable(Unit movable) {
+
+    }
+    // 매개변수 자료형 변경 가능
+    public void registerCollisionEventListener(Unit listener) {
+
+    }
+
+    public void update() {
+
+    }
+}
