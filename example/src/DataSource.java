56ccde6cbbdac5ce0b0dad2413a5f426cbbf8b44
kimgun95 <harrison1995@naver.com>
2021-03-16 23:49:22 +0900

    ex

---
+++ example/src/DataSource.java
@@ -0,0 +1,8 @@
+import java.util.Collection;
+
+public final class DataSource {
+    // 멤버 변수 생략
+    public void MergTo(Collection<Data> dataset) {
+        // 소스로부터 모든 데이터를 얻어와 중복 없이 dataset에 넣는다.
+    }
+}
